d43dd173ea9a6ba2ec997f04e3a40fa8273a90ea
MN237 <55341574+MN-237@users.noreply.github.com>
2024-09-16 21:47:11 +0200

    Update esppac.cpp

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -76,7 +76,7 @@ void PanasonicAC::update_current_temperature(int8_t temperature) {
     return;
   }
 
-  this->current_temperature = temperature;
+  this->current_temperature = temperature - 15;
 }
 
 void PanasonicAC::update_target_temperature(uint8_t raw_value) {
